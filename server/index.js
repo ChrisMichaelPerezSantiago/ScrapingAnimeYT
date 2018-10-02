93c59d63838c1245364a3fd40b29d3f655e5c746
Chris Michael <chris.perez@upr.edu>
2018-10-01 23:44:21 -0400

    information about recent series has been obtained.

--- server/index.js
+++ server/index.js
@@ -16,20 +16,50 @@ const getRecentChapters = async () =>{
         const link = $item.find('a.capitulos-grid__item__link').attr('href');
         const image = $item.find('img.capitulos-grid__item__img').attr('src');
         const synopsis = $item.find('p.slider-home__synopsis').text().trim();
-        const recentSeries = {
+        const recentChapters = {
             title,
             synopsis,
             image,
             link
         }
+        const recentChaptersRef = db.collection('recentchapters').doc();
+        promises.push(recentChaptersRef.set(recentChapters));
+    });
+    await Promise.all(promises);
+}
 
+const getRecentSeries = async () =>{
+    const url = 'https://www.animeyt.tv';
+    const response = await fetch(url);
+    const body = await response.text();
+    const $ = cheerio.load(body);
+
+    const promises = [];
+
+    $('div.anime__data').each((index , item) =>{
+        const $item = $(item);
+        const title = $item.find('h3.anime__title').text();
+        const type = $item.find('h3.anime__title').find('small').text().replace(/[()]/g , '');
+        const date = $item.find('div.anime__date').children().remove().end().text().trim();
+        const gender = []
+        $item.find('div.anime__genres').children().each((index , item) =>{
+            const $item = $(item);
+            gender[index] = $item.text();
+        });
+        const tags = []
+        $item.find('div.anime__tags').children().each((index , item) =>{
+            const $item = $(item);
+            tags[index] = $item.text();
+        });
+        const recentSeries = {
+            title,
+            date,
+            tags,
+            gender,
+            type
+        }
         const recentSeriesRef = db.collection('recentseries').doc();
         promises.push(recentSeriesRef.set(recentSeries));
     });
-    await Promise.all(promises);
-    console.log('done...');
-    
+    await(Promise.all(promises));
 }
-
-getRecentChapters();
-
